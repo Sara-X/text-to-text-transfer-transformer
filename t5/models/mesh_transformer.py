55e9de8e523c59598f19ce5466b4bb75ab4b6a5f
Sara-X <43844100+Sara-X@users.noreply.github.com>
2020-05-09 17:13:35 +0800

    Update mesh_transformer.py

--- t5/models/mesh_transformer.py
+++ t5/models/mesh_transformer.py
@@ -26,14 +26,10 @@ import tensorflow.compat.v1 as tf
 import tensorflow_datasets as tfds
 
 
-def valid_vocabulary(vocabulary):
-  """Tests that a vocabulary is valid to pass to Mesh Tensorflow transformer."""
-   # Mesh TF allows for a (inputs_vocab, targets_vocab) tuple
-  if not isinstance(vocabulary, tuple):
-    vocabulary = (vocabulary,)
-  for v in vocabulary:
-    if not isinstance(v, t5.data.vocabularies.Vocabulary):
-      raise ValueError("vocabulary must be a t5.data.vocabularies.Vocabulary")
+@gin.configurable()
+def get_sentencepiece_model_path(mixture_or_task_name):
+  return t5.data.get_mixture_or_task(
+      mixture_or_task_name).sentencepiece_model_path
 
 
 @gin.configurable()
@@ -53,7 +49,7 @@ def mesh_train_dataset_fn(
       appropriate registry. Must be specified via gin.
     sequence_length: dict mapping feature key to the int length for that feature
       the max sequence length.
-    vocabulary: a t5.data.vocabularies.Vocabulary.
+    vocabulary: a SentencePieceVocabulary.
     dataset_split: string, which split of the dataset to load. In most cases
       this should be "train".
     use_cached: bool, whether to load the cached version of this dataset.
@@ -61,7 +57,8 @@ def mesh_train_dataset_fn(
   Returns:
     A tf.data.Dataset of preprocessed, tokenized, and batched examples.
   """
-  valid_vocabulary(vocabulary)
+  if not isinstance(vocabulary, t5.data.SentencePieceVocabulary):
+    raise ValueError("vocabulary must be a SentencePieceVocabulary")
 
   mixture_or_task = t5.data.get_mixture_or_task(mixture_or_task_name)
 
@@ -85,8 +82,7 @@ def mesh_eval_dataset_fn(
     vocabulary,
     dataset_split,
     num_eval_examples=None,
-    use_cached=False,
-    pack=False):
+    use_cached=False):
   """Returns all tf.data.Datasets for evaluation on a given mixture.
 
   This uses the format required for utils.run's `eval_dataset_fn` argument in
@@ -97,18 +93,17 @@ def mesh_eval_dataset_fn(
       appropriate registry. Must be specified via gin.
     sequence_length: dict mapping feature key to the int length for that feature
       the max sequence length.
-    vocabulary: a t5.data.vocabularies.Vocabulary.
+    vocabulary: a SentencePieceVocabulary.
     dataset_split: string, which split of the dataset to load.
     num_eval_examples: maximum number of examples per task to use for continuous
       eval. If None, use all examples.
     use_cached: bool, whether to load the cached version of this dataset.
-    pack: a boolean, whether to pack examples. This is useful for perplexity
-      evals but should not be used for iterative decoding.
 
   Returns:
     A list of mesh_tensorflow.transformer.dataset.EvalDataset tuples.
   """
-  valid_vocabulary(vocabulary)
+  if not isinstance(vocabulary, t5.data.SentencePieceVocabulary):
+    raise ValueError("vocabulary must be a SentencePieceVocabulary")
 
   mixture_or_task = t5.data.get_mixture_or_task(mixture_or_task_name)
 
@@ -126,7 +121,7 @@ def mesh_eval_dataset_fn(
     ds = transformer_dataset.pack_or_pad(
         ds,
         sequence_length,
-        pack=pack,
+        pack=False,
         feature_keys=tuple(task.output_features),
         ensure_eos=True)
     if num_eval_examples is not None:
@@ -171,51 +166,3 @@ def tsv_dataset_fn(
       dataset_split=dataset_split,
       append_eos=True,
       eos_id=1)
-
-
-@gin.configurable()
-def get_vocabulary(mixture_or_task_name):
-  """Get the appropriate value for the utils.run.vocabulary argument.
-
-  Args:
-    mixture_or_task_name: string, an identifier for a Mixture or Task in the
-      appropriate registry. Must be specified via gin.
-
-  Returns:
-    Either a single t5.data.vocabularies.Vocabulary or a tuple of
-    t5.data.vocabularies.Vocabulary for inputs and targets.
-  """
-  provider = t5.data.get_mixture_or_task(mixture_or_task_name)
-  features = provider.output_features
-  if "inputs" in features and "targets" in features:
-    return (features["inputs"].vocabulary, features["targets"].vocabulary)
-  else:
-    return provider.get_vocabulary()
-
-
-@gin.configurable()
-def get_sentencepiece_model_path(mixture_or_task_name):
-  """Return the SentencePiece model path for a given mixture or task.
-
-  DEPRECATED. Please pass the vocabulary directly to utils.run instead.
-
-  Args:
-    mixture_or_task_name: string, an identifier for a Mixture or Task in the
-      appropriate registry. Must be specified via gin.
-
-  Returns:
-    Path to a SentencePiece model file.
-  """
-  warnings.warn(
-      "get_sentencepiece_model_path is deprecated. Please pass the mixture or "
-      "task vocabulary directly to the Mesh TensorFlow Transformer instead."
-  )
-  provider = t5.data.get_mixture_or_task(mixture_or_task_name)
-  vocabulary = provider.get_vocabulary()
-  if not isinstance(vocabulary,
-                    t5.data.sentencepiece_vocabulary.SentencePieceVocabulary):
-    raise ValueError(
-        "get_sentencepiece_model_path was called for a provider that does not "
-        "use a SentencePieceVocabulary."
-    )
-  return vocabulary.sentencepiece_model_file
